40a774970738238a5c49b145d08579fbf353830b
17josue <jjcorrea@espe.ede.ec>
2026-06-04 21:41:27 -0500

    comentar

---
+++ Factorial.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<string>
+#include<math.h>
+
+int main(){
+    printf("aaaaaaaa la bestia y esa m10");
+    return 0;
+}
